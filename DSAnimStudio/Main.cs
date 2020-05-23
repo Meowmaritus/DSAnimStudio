49165cd59fb76847a0755385fcd3e8cdd80ab787
Meowmaritus <Meowmaritus@gmail.com>
2020-05-23 01:21:54 -0500

    Fix 3D panning

--- DSAnimStudio/Main.cs
+++ DSAnimStudio/Main.cs
@@ -38,7 +38,7 @@ namespace DSAnimStudio
 
         public static string Directory = null;
 
-        public const string VERSION = "Version 2.1";
+        public const string VERSION = "Version 2.1.1";
 
         public static bool FIXED_TIME_STEP = false;
 
